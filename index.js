33ebfd87315657ca16632974a484d30a5a1abcee
SID-JET <76100309+SID-JET@users.noreply.github.com>
2021-03-02 09:34:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 document.querySelector(".big-heading").addEventListener("click", function(){var audio = new Audio("laly.ogg");
 audio.play();
 animation();
-$("h1").text("I Love you so much Siddy bear!!");                                                                            
+if($("h1").text()=="I Love you so much Sid!!!"){$("h1").text("I Love you so much Siddy bear!!");}
+else{$("h1").text("I Love you so much Sid!!!");}
+                                                                            
 });
 
 function animation(){
